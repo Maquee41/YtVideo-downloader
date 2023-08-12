90bab315bc9079b5fdf718ff2caa675292b67e31
Egor Golikov <egor.golikov2006@inbox.ru>
2023-08-12 19:31:27 +0300

    release V.1

---
+++ README.md
@@ -0,0 +1,30 @@
+# YtVideo Downloader
+## Description
+
+This script can download all videos from YouTube. To start you need only `yt-dlp` and `FFmpeg`
+
+## Suported Resolution
+
+| 2160p | 1440p | 1080p | 720p | 480p | 360p | 240p | 144p |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| ✔️ | ✔️ | ✔️ | ✔️ | ✔️ | ✔️ | ✔️ | ✔️ |
+
+Check supported resolution in the quality tab (not all videos support all resolutions in YouTube)
+
+![Alt text](https://user-life.com/uploads/posts/2021-06/1624555852_kak-izmenit-kachestvo-video-na-youtube3.png)
+
+
+## Installation
+
+1. Create new folder. Move there `main.py`
+2. Install [FFmpeg](https://ffmpeg.org/)  You need only `ffmpeg.exe`. Find it in `\ffmpeg**version**\bin\` and move to a folder with main.py
+3. Create virtual environment
+4. Download yt-dlp | `pip install yt-dlp`
+5. That's all. Remember! you can use it only with Python interpreter
+
+## CMD interface
+
+- At first you should enter your video link
+- Then you write your future video title
+- And the last you can choose resolution  
+example: `Select resolution 2160/1440/1080/720/480/360/240/144: 1080`)
---
+++ main.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+
+
+class Programm():
+    def __init__(self):
+        self.link = input('Enter yt video link: ')
+        self.name_output = input('Enter video title: ')
+        self.resolution = input('Select resolution 2160/1440/1080/720/480/360/240/144: ')
+        self.file_name = f'{self.name_output}.mp4'
+
+
+    def download_audio(self):
+        command = ['yt-dlp', '-f ba[ext=m4a]','-oaudio.m4a' , self.link]
+        subprocess.call(command)
+
+    def download_video(self):
+        command = ['yt-dlp', f'-f "bv[height<=?{self.resolution}]', '-ovideo.webm', self.link]
+        subprocess.call(command)
+
+    def audio_plus_video(self):
+        subprocess.call(f'ffmpeg -i video.webm -i audio.m4a -c copy {self.name_output}.mp4')
+
+    
+    def check_width_and_height(self):
+        subprocess.call(f'ffprobe -v error -show_entries stream=width,height -of default=noprint_wrappers=1 {self.file_name}')
+
+    @staticmethod
+    def clear_temporary():
+        os.remove('audio.m4a')
+        os.remove('video.webm')
+
+
+def execute_main():
+    app = Programm()
+    app.download_audio()
+    app.download_video()
+    app.audio_plus_video()
+    app.check_width_and_height()
+    app.clear_temporary()
+
+
+
+if __name__ == '__main__':
+    execute_main()
